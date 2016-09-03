59801437bb002ae03426a855e02e0c1c18e3058f
Nigel Heap <nigel@nigelheap.com>
2016-09-03 16:37:19 -0300

    reset repo

---
+++ src/NigelHeap/Roles/Exceptions/AccessDeniedException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NigelHeap\Roles\Exceptions;
+
+use Exception;
+
+class AccessDeniedException extends Exception
+{
+    //
+}
---
+++ src/NigelHeap/Roles/Models/Role.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NigelHeap\Roles\Models;
+
+use NigelHeap\Roles\Traits\Slugable;
+use Illuminate\Database\Eloquent\Model;
+use NigelHeap\Roles\Traits\RoleHasRelations;
+use NigelHeap\Roles\Contracts\RoleHasRelations as RoleHasRelationsContract;
+
+class Role extends Model implements RoleHasRelationsContract
+{
+    use Slugable, RoleHasRelations;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'slug', 'description', 'level'];
+
+    /**
+     * Create a new model instance.
+     *
+     * @param array $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if ($connection = config('roles.connection')) {
+            $this->connection = $connection;
+        }
+    }
+}
